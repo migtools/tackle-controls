227b13cd80a632f33056b138ab2f438f03bd9dd2
Marco Rizzi <mrizzi@users.noreply.github.com>
2021-02-24 20:51:04 +0100

    Refactored properties eval (#18)

--- src/main/resources/issue_create.sh
+++ src/main/resources/issue_create.sh
@@ -1,1 +1,8 @@
+echo $GH_ISSUE_TITLE
+echo $NUMBER
+echo $DESCRIPTION
+JIRA_ISSUE_ID=$(echo $GH_ISSUE_TITLE|cut -d] -f1|sed 's/[[]//g')
+TITLE=$(echo $GH_ISSUE_TITLE|cut -d] -f2})
+echo $JIRA_ISSUE_ID
+echo $TITLE
 curl -X POST -H "Content-Type: application/json" -H "Authorization: Bearer "$BEARER https://issues.redhat.com/rest/api/2/issue/ -d "{ \"fields\": { \"project\": { \"key\": \"TACKLE\" }, \"parent\": { \"key\": \"$JIRA_ISSUE_ID\" }, \"summary\": \"[tackle-controls#$NUMBER]$TITLE\", \"description\": \"$DESCRIPTION\", \"issuetype\": { \"id\": 5 } } }"
