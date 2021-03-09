db96e9f230b88b8430c10069fb27a00d68d5d737
m-brophy <mbrophy@redhat.com>
2021-03-09 16:55:40 +0000

    TACKLE-6: Stakeholder Group resource classes

---
+++ src/main/java/io/tackle/controls/resources/StakeholderGroupListFilteredResource.java
@@ -0,0 +1,48 @@
+package io.tackle.controls.resources;
+
+import io.tackle.commons.resources.ListFilteredResource;
+import io.tackle.controls.entities.StakeholderGroup;
+import org.jboss.resteasy.links.LinkResource;
+
+import javax.ws.rs.DefaultValue;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+
+@Path("stakeholder-group")
+public class StakeholderGroupListFilteredResource implements ListFilteredResource<StakeholderGroup> {
+
+    @Override
+    public Class<StakeholderGroup> getPanacheEntityType() {
+        return StakeholderGroup.class;
+    }
+
+    @GET
+    @Path("")
+    @Produces({"application/json"})
+    @LinkResource(
+            entityClassName = "io.tackle.controls.entities.StakeholderGroup",
+            rel = "list"
+    )
+    public Response list(@QueryParam(QUERY_PARAM_SORT) @DefaultValue(DEFAULT_VALUE_SORT) List var1,
+                         @QueryParam(QUERY_PARAM_PAGE) @DefaultValue(DEFAULT_VALUE_PAGE) int var2,
+                         @QueryParam(QUERY_PARAM_SIZE) @DefaultValue(DEFAULT_VALUE_SIZE) int var3,
+                         @Context UriInfo var4) throws Exception {
+        return ListFilteredResource.super.list(var1, var2, var3, var4, false);
+    }
+
+    @Path("")
+    @GET
+    @Produces({"application/hal+json"})
+    public Response listHal(@QueryParam(QUERY_PARAM_SORT) @DefaultValue(DEFAULT_VALUE_SORT) List var1,
+                            @QueryParam(QUERY_PARAM_PAGE) @DefaultValue(DEFAULT_VALUE_PAGE) int var2,
+                            @QueryParam(QUERY_PARAM_SIZE) @DefaultValue(DEFAULT_VALUE_SIZE) int var3,
+                            @Context UriInfo var4) throws Exception {
+        return ListFilteredResource.super.list(var1, var2, var3, var4, true);
+    }
+}
