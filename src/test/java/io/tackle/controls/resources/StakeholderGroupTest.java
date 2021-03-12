60c7bf1082d573e68f9df4910b429087e852fb77
m-brophy <mbrophy@redhat.com>
2021-03-12 17:11:43 +0000

    TACKLE-6: stakeholder group test class added

---
+++ src/test/java/io/tackle/controls/resources/StakeholderGroupTest.java
@@ -0,0 +1,65 @@
+package io.tackle.controls.resources;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import io.quarkus.test.common.QuarkusTestResource;
+import io.quarkus.test.common.ResourceArg;
+import io.quarkus.test.junit.QuarkusTest;
+import io.restassured.RestAssured;
+import io.restassured.config.ObjectMapperConfig;
+import io.restassured.parsing.Parser;
+import io.tackle.commons.testcontainers.KeycloakTestResource;
+import io.tackle.commons.testcontainers.PostgreSQLDatabaseTestResource;
+import io.tackle.commons.tests.SecuredResourceTest;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.containsInRelativeOrder;
+import static org.hamcrest.Matchers.is;
+
+@QuarkusTest
+@QuarkusTestResource(value = PostgreSQLDatabaseTestResource.class,
+        initArgs = {
+                @ResourceArg(name = PostgreSQLDatabaseTestResource.DB_NAME, value = "controls_db"),
+                @ResourceArg(name = PostgreSQLDatabaseTestResource.USER, value = "controls"),
+                @ResourceArg(name = PostgreSQLDatabaseTestResource.PASSWORD, value = "controls")
+        }
+)
+@QuarkusTestResource(value = KeycloakTestResource.class,
+        initArgs = {
+                @ResourceArg(name = KeycloakTestResource.IMPORT_REALM_JSON_PATH, value = "keycloak/quarkus-realm.json"),
+                @ResourceArg(name = KeycloakTestResource.REALM_NAME, value = "quarkus")
+        }
+)
+public class StakeholderGroupTest extends SecuredResourceTest {
+
+    @BeforeAll
+    public static void init() {
+        PATH = "/stakeholder-group";
+    }
+
+    @Test
+    public void testStakeholderGroupListEndpoint() {
+        RestAssured.defaultParser = Parser.JSON;
+        RestAssured.config
+                .objectMapperConfig(new ObjectMapperConfig().jackson2ObjectMapperFactory((type, s) -> new ObjectMapper()
+                                .registerModule(new Jdk8Module())
+                                .registerModule(new JavaTimeModule())
+                        /*.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)*/));
+
+        given()
+                .accept("application/json")
+                .queryParam("sort", "id")
+                .when().get(PATH)
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("size()", is(3),
+                        "id", containsInRelativeOrder(52, 53, 54),
+                        "name", containsInRelativeOrder("Managers", "Engineers", "Marketing"),
+                        "createUser", containsInRelativeOrder("<pre-filled>", "<pre-filled>", "<pre-filled>")
+                );
+    }
+}
