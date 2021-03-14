7377b63a8f924d3cedb383944a74e14250c42985
Marco Rizzi <mrizzi@users.noreply.github.com>
2021-03-14 18:15:40 +0100

    Fix Flyway migration scripts (#62)
    * Add test to check Flyway is broken
    
    * Fixed flyway migrations script for tags
    
    * Changed 'Java EE' language to 'Java'

--- src/main/resources/db/migration/V20210309.4__insert_tag.sql
+++ src/main/resources/db/migration/V20210309.4__insert_tag.sql
@@ -1,28 +1,112 @@
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'COTS', 18, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'In house', 18, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'SaaS', 18, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Boston (USA)', 19, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'London (UK)', 19, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Paris (FR)', 19, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Sydney (AU)', 19, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'DB2', 20, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'MongoDB', 20, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Oracle', 20, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Postgresql', 20, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'SQL Server', 20, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'C# ASP .Net', 21, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'C++', 21, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'COBOL', 21, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Java EE', 21, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Javascript', 21, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Python', 21, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'RHEL 8', 22, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Windows Server 2016', 22, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Z/OS', 22, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'EAP', 23, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'JWS', 23, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Quarkus', 23, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Spring Boot', 23, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'Tomcat', 23, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'WebLogic', 23, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
-INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted) VALUES (nextval('hibernate_sequence'), 'WebSphere', 23, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false);
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'COTS', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Application Type';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'In house', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Application Type';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'SaaS', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Application Type';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Boston (USA)', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Data Center';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'London (UK)', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Data Center';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Paris (FR)', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Data Center';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Sydney (AU)', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Data Center';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'DB2', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Database';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'MongoDB', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Database';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Oracle', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Database';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Postgresql', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Database';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'SQL Server', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Database';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'C# ASP .Net', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Language';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'C++', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Language';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'COBOL', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Language';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Java', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Language';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Javascript', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Language';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Python', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Language';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'RHEL 8', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Operating System';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Windows Server 2016', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Operating System';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Z/OS', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Operating System';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'EAP', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Runtime';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'JWS', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Runtime';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Quarkus', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Runtime';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Spring Boot', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Runtime';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'Tomcat', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Runtime';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'WebLogic', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Runtime';
+INSERT INTO tag (id, name, tagType_id, createUser, createTime, updateUser, updateTime, deleted)
+SELECT nextval('hibernate_sequence'), 'WebSphere', id, '<shipped-data>', CURRENT_TIMESTAMP, '<shipped-data>', CURRENT_TIMESTAMP, false
+FROM tag_type
+WHERE name = 'Runtime';
--- src/test/java/io/tackle/controls/resources/issues/Issue9Test.java
+++ src/test/java/io/tackle/controls/resources/issues/Issue9Test.java
@@ -1,4 +1,4 @@
-package io.tackle.controls.issues;
+package io.tackle.controls.resources.issues;
 
 import io.quarkus.test.common.QuarkusTestResource;
 import io.quarkus.test.common.ResourceArg;
